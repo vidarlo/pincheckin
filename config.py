7528b0f96bb312a4c0bed96db623f47f2f78355d
Vidar Løkken <vidarlo@bitsex.net>
2021-01-31 19:17:00 +0100

    Updated config

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ except Exception as inst:
 def DBFile():
     return(config['DB']['SQLFile'])
 
+def listen_ip():
+    return(config['Server']['listen_ip'])
+
+def listen_port():
+    return(config['Server']['listen_port'])
+
 
     
           
