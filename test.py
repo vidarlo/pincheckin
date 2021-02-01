005a916c939ce5afb3d75c87ee2cc41261274eff
Vidar Løkken <vidarlo@bitsex.net>
2021-02-01 07:38:43 +0100

    Example

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import config
 db_file = config.DBFile()
 
 conn = db.create_connection(db_file)
-#new_user = db.new_user(conn,'VLO','vl@bitsex.net','+4796628780')
-checkin = db.insert_checkin(conn, 'VLO')
-checkout = db.insert_checkout(conn,'VLO')
+#new_user = db.new_user(conn,'HAG','foo@example.com','+12345678901')
+checkin = db.insert_checkin(conn, 'HAG')
+checkout = db.insert_checkout(conn,'HAG')
 #print(new_user)
 print(checkin)
 print(checkout)
